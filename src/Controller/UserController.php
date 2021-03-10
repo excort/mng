86f7f60469a54f24ad00d9fcdb3d83966c33e5f3
Vladimir Moroz <excort13@mail.ru>
2021-03-11 01:26:28 +0200

    security

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController  extends AbstractController
 {
@@ -20,9 +21,9 @@ class UserController  extends AbstractController
     }
 
     #[Route('/user/login', name: 'user_login', methods:["POST"])]
-    public function login(Request $request): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        dump(__METHOD__, $this->getUser());die();
+        dump(__METHOD__, $this->getUser(), $authenticationUtils->getLastUsername());die();
     }
 
     #[Route('/user/list', name: 'user_list')]
