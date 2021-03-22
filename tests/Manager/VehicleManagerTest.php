942bcbb4434811105de465a785471ef6e6b4dbf7
Vladimir Moroz <excort13@mail.ru>
2021-03-23 00:38:31 +0200

    first test

--- tests/Manager/VehicleManagerTest.php
+++ tests/Manager/VehicleManagerTest.php
@@ -17,35 +17,38 @@ class VehicleManagerTest extends TestCase
 
     private VehicleManager $vehicleManager;
 
-//    public function setUp(): void
-//    {
-//        $this->mongoProviderMock = $this->createMock(MongoProvider::class);
-//        $this->registrationProviderMock = $this->createMock(RegistrationProvider::class);
-//        $this->vehicleProviderMock = $this->createMock(VehicleProvider::class);
-//
-//        $this->vehicleManager = new VehicleManager(
-//            $this->mongoProviderMock,
-//            $this->registrationProviderMock,
-//            $this->vehicleProviderMock
-//        );
-//    }
-
-//    /**
-//     * @dataProvider addWithCarCategoryProvider
-//     */
-    //#[Assert\Uuid]
-    public function getVehicleByUserTest(/*bool $isAdmin*/)
+    public function setUp(): void
     {
-//        $userMock = $this->createMock(User::class);
-//        $userMock
-//            ->expects($this->any())
-//            ->method('isAdmin')
-//            ->willReturn($isAdmin);
-//        ;
-//
-//        $this->vehicleManager->getVehicleByUser($userMock, null, 1, 5);
+        $this->mongoProviderMock = $this->createMock(MongoProvider::class);
+        $this->registrationProviderMock = $this->createMock(RegistrationProvider::class);
+        $this->vehicleProviderMock = $this->createMock(VehicleProvider::class);
 
+        $this->vehicleManager = new VehicleManager(
+            $this->mongoProviderMock,
+            $this->registrationProviderMock,
+            $this->vehicleProviderMock
+        );
+    }
+
+    /**
+     * @dataProvider addWithCarCategoryProvider
+     */
+    //#[Assert\Uuid]
+    public function testGetVehicleByUserTest(bool $isAdmin)
+    {
+        $userMock = $this->createMock(User::class);
+        $userMock
+            ->expects($this->any())
+            ->method('isAdmin')
+            ->willReturn($isAdmin);
+        ;
+// https://phpunit.readthedocs.io/ru/latest/writing-tests-for-phpunit.html
+// https://symfony.com/doc/current/testing.html#phpunit-configuration
+
+        $this->vehicleManager->getVehicleByUser($userMock, null, 1, 5);
+
+        $ttt = 5;
         // assert that your calculator added the numbers correctly!
-        $this->assertEquals(42, 42);
+        $this->assertEquals(42, $ttt);
     }
 }
