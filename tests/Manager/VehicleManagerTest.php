6667f00034193595b324ba37fe1b679e196aa9a4
Vladimir Moroz <excort13@mail.ru>
2021-03-23 00:08:42 +0200

    add phpunit

---
+++ tests/Manager/VehicleManagerTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Tests\Manager;
+
+use App\Entity\User;
+use App\Manager\RegistrationProvider;
+use App\Manager\VehicleManager;
+use App\Manager\VehicleProvider;
+use App\Service\MongoProvider;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class VehicleManagerTest extends TestCase
+{
+    private MongoProvider $registrationProviderMock;
+    private RegistrationProvider $vehicleProviderMock;
+    private VehicleProvider $mongoProviderMock;
+
+    private VehicleManager $vehicleManager;
+
+//    public function setUp(): void
+//    {
+//        $this->mongoProviderMock = $this->createMock(MongoProvider::class);
+//        $this->registrationProviderMock = $this->createMock(RegistrationProvider::class);
+//        $this->vehicleProviderMock = $this->createMock(VehicleProvider::class);
+//
+//        $this->vehicleManager = new VehicleManager(
+//            $this->mongoProviderMock,
+//            $this->registrationProviderMock,
+//            $this->vehicleProviderMock
+//        );
+//    }
+
+//    /**
+//     * @dataProvider addWithCarCategoryProvider
+//     */
+    //#[Assert\Uuid]
+    public function getVehicleByUserTest(/*bool $isAdmin*/)
+    {
+//        $userMock = $this->createMock(User::class);
+//        $userMock
+//            ->expects($this->any())
+//            ->method('isAdmin')
+//            ->willReturn($isAdmin);
+//        ;
+//
+//        $this->vehicleManager->getVehicleByUser($userMock, null, 1, 5);
+
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals(42, 42);
+    }
+}
